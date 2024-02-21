0bedbb3eb00a2f569a2ad276cee90f57d5f37a5b
Sajid Anam Ifti <info.sajidifti@gmail.com>
2024-02-21 17:51:46 +0600

    created custom user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Custom
+AUTH_USER_MODEL = "core.User"
